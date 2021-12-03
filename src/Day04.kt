3397139a433a24a25b4d6c5cd5e869eef93c7ab2
Tiperciuc Gabriela <gabriela.tiperciuc18@gmail.com>
2021-12-03 08:25:19 +0200

    Day04 preparin

---
+++ src/Day04.kt
@@ -0,0 +1,18 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
